19a1bcb16366834a32238973591f1ba54c8d632f
Dio <dio@Mac-Mini-08s-Mac-mini.local>
2016-10-27 19:09:16 -0200

    TEST IMGUR UPLOAD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser');
+var imgur = require('imgur');
 
 var app = express();
 
@@ -24,8 +24,29 @@ app.get('*', function (req, res) {
     res.render('index');
 });
 
+
+// imgur
+imgur.setClientId('d44fc08d3815abe');
+
+imgur.getClientId();
+
+imgur.saveClientId()
+    .then(function () {
+        console.log('Saved.');
+    })
+    .catch(function (err) {
+        console.log(err.message);
+    });
+
+imgur.setCredentials('diogos.go@gmail.com', '***', 'aCs53GSs4tga0ikp');
+
+// post
+app.post('/upload', function (req, res) {
+	console.log(req.body);
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('App is running on http://localhost:' + port);
 });
--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -2,42 +2,83 @@
 
 // fake add image
 function handleFileSelect(e) {
-    var files = e.target.files;
+    var reader = new FileReader();  
+    var file = document.getElementById('upload-photo');
 
-    // loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
+    reader.addEventListener('load', function() {
+        var span = document.createElement('span');
+        span.innerHTML = [
+            '<img src="', 
+            e.target.result,
+            '"/>'
+        ].join('');
 
-        // only process image files.
-        if (!f.type.match('image.*')) {
-        continue;
-    }
+        list.insertBefore(span, list.childNodes[0]);
+    }, false);
 
-    var reader = new FileReader();
-
-    // Closure to capture the file information.
-    reader.onload = (function(theFile) {
-        return function(e) {
-            // Render thumbnail.
-            var list = document.getElementById('list');
-
-            var span = document.createElement('span');
-            span.innerHTML = [
-                '<img src="', 
-                e.target.result,
-                '"/>'
-            ].join('');
-
-            list.insertBefore(span, list.childNodes[0]);
-        };
-    })(f);
-
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
+    if (file) {
+        reader.readAsDataURL(file);
     }
 }
 
 (function() {
+    var list = document.getElementById('list');
     var uploadInput = document.getElementById('upload-photo');
 
-    uploadInput.addEventListener('change', handleFileSelect, false);
+    uploadInput.addEventListener('change', handleFileSelect);
 })();
+
+
+/// old
+
+// 'use strict';
+
+// // fake add image
+// function handleFileSelect(e) {
+//     var files = e.target.files;
+
+//     // loop through the FileList and render image files as thumbnails.
+//     for (var i = 0, f; f = files[i]; i++) {
+
+//         // only process image files.
+//         if (!f.type.match('image.*')) {
+//             continue;
+//         }
+
+//         var reader = new FileReader();
+
+//         // Closure to capture the file information.
+//         reader.onload = (function(theFile) {
+//             return function(e) {
+//                 // Render thumbnail.
+//                 var span = document.createElement('span');
+//                 span.innerHTML = [
+//                     '<img src="', 
+//                     e.target.result,
+//                     '"/>'
+//                 ].join('');
+
+//                 list.insertBefore(span, list.childNodes[0]);
+//             };
+//         })(f);
+
+//         // Read in the image file as a data URL.
+//         reader.readAsDataURL(f);
+
+//         var request = new XMLHttpRequest();
+
+//         request.open('POST', '/upload', true);
+//         request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+//         request.send(JSON.stringify(reader.readAsDataURL(f)));
+
+//         console.log(reader.readAsDataURL(f));
+//     }
+// }
+
+// (function() {
+    
+//     var list = document.getElementById('list');
+//     var uploadInput = document.getElementById('upload-photo');
+
+//     uploadInput.addEventListener('change', handleFileSelect, false);
+// })();
