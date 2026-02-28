70c3a2fd2c3b89282c3163d455944a1c6639008c
Diogo Fernandes <diogo.ff@icloud.com>
2026-02-28 17:48:32 -0300

    Remove sentry

--- astro.config.ts
+++ astro.config.ts
@@ -2,27 +2,14 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel";
-import sentry from "@sentry/astro";
 import { loadEnv } from "vite";
 
-const { PUBLIC_BASE_URL, SENTRY_AUTH_TOKEN } = loadEnv(
-	process.env.NODE_ENV || "development",
-	process.cwd(),
-	"",
-);
+const { PUBLIC_BASE_URL } = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
 	site: PUBLIC_BASE_URL,
-	integrations: [
-		svelte(),
-		sentry({
-			org: "gugugram",
-			project: "elo7",
-			// store your auth token in an environment variable
-			authToken: SENTRY_AUTH_TOKEN,
-		}),
-	],
+	integrations: [svelte()],
 	adapter: vercel(),
 	output: "server",
 	security: {
