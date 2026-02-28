026c188e7f8dd1ea81ae1fe4f6611e4b085752d3
Diogo Fernandes <diogo.ff@icloud.com>
2026-02-28 17:43:14 -0300

    Get env variables using loadEnv from vite

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel";
-
 import sentry from "@sentry/astro";
+import { loadEnv } from "vite";
+
+const { PUBLIC_BASE_URL, SENTRY_AUTH_TOKEN } = loadEnv(
+	process.env.NODE_ENV || "development",
+	process.cwd(),
+	"",
+);
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.PUBLIC_BASE_URL,
+	site: PUBLIC_BASE_URL,
 	integrations: [
 		svelte(),
 		sentry({
 			org: "gugugram",
 			project: "elo7",
 			// store your auth token in an environment variable
-			authToken: process.env.SENTRY_AUTH_TOKEN,
+			authToken: SENTRY_AUTH_TOKEN,
 		}),
 	],
 	adapter: vercel(),
