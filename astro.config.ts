3bcefe82d05f7ff2500be076930cfe3c9557ae0d
Diogo Fernandes <diogo.ff@icloud.com>
2026-02-28 16:56:08 -0300

    Add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   output: "server",
 });
